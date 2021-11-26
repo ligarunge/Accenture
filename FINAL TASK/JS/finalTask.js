46b737bcaf50c0f7208b59e1e70e21febbdd4873
ligarunge <liga.runge@gmail.com>
2021-11-26 16:17:37 +0200

    final task

--- FINAL TASK/JS/finalTask.js
+++ FINAL TASK/JS/finalTask.js
@@ -16,4 +16,36 @@
     }, false);
   })();
 
- 
+ document.getElementById('sendMessage').addEventListener('click', function() {
+    var today = new Date();
+    var date = today.getFullYear()+"."+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes();
+    var dateTime = date+' '+time;
+    
+   n = today.toLocaleDateString();
+   n = n.replace(/\//g, '.');
+   var time = today.toLocaleTimeString();
+
+   document.getElementById('time').innerHTML = n + ' ' + time;
+
+     const message = document.getElementById('message');
+     const username = document.getElementById('usern');
+     const newMessage = `<li class="list-group-item">${message.value}</li>`;
+     const newUsername = `<li class="list-group-item">${username.value}</li>`
+     document.getElementById('messageSent').innerHTML += newMessage;
+     document.getElementById('usernameSent').innerHTML += newUsername;
+ })
+
+
+
+
+//var date = new Date();
+// get the date as a string
+//var n = date.toDateString();
+// get the time as a string
+//var time = date.toLocaleTimeString();
+
+// find the html element with the id of time
+// set the innerHTML of that element to the date a space the time
+//document.getElementById('time').innerHTML = n + ' ' + time;
+//<div id='time'></div>
